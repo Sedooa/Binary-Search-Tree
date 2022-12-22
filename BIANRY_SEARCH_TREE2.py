4f21a52087394b1c3c87f828710bd745d5486b8d
SEDAT ANIK <109542820+Sedooa@users.noreply.github.com>
2022-12-22 13:47:30 +0300

    Add files via upload

---
+++ BIANRY_SEARCH_TREE2.py
@@ -0,0 +1,23 @@
+
+[7, 5, 1, 8, 3, 6, 0, 9, 4, 2]  Binary Search Tree :
+
+Root:5
+
+                                 5
+                                / \
+             Step 1:          3     7
+                            /  \    / \
+           Step 2:         1    4   6   8
+                         /   \            \
+       Step 3:          0     2             9
+                      
+
+Step 1: '3' is placed on the Left side of '5' because of being less than '5' ; '7' is placed on the Right side of '5' because of being greater than '5'
+
+Step 2: '1' is placed on the Left side of '3' because of being less than '3' ; '4' is placed on the Right side of '3' because of being greater than '3' 
+
+Step 2: '6' is placed on the Left side of '7' because of being less than '7' ; '8' is placed on the Right side of '7' because of being greater than '7'
+
+Step 3: '0' is placed on the Left side of '1' because of being less than '1' ; '2' is placed on the Right side of '1' because of being greater than '1'
+
+Step 3: '9' is placed on the Right side of '8' because of being greater than '8'
